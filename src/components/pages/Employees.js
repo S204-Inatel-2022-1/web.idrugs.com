b74adf2d454039cb38a12cbaa2f87aea580d8b78
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-05-03 17:28:11 -0300

    "method GET Employees"

--- src/components/pages/Employees.js
+++ src/components/pages/Employees.js
@@ -1,6 +1,36 @@
+import React, { useState, useEffect} from "react";
+
 function Employees() {
+
+    const [data, setData] = useState([{}])
+
+    useEffect(() => {
+        fetch("/user").then(
+            res => res.json()
+        ).then(
+            data => {
+                setData(data)
+                console.log(data)
+            }
+        )
+    }, [])
+
     return (
-        <h1>Funcionários</h1>
+        <div>
+            {data.map((item) => (
+                <ul>
+                    <li key={item.id}>
+                        nome:{item.name}
+                    </li>
+
+                    <li key={item.id}>
+                        email:{item.email}
+                    </li>
+
+                </ul>
+
+            ))}
+        </div>
     )
 }
 
