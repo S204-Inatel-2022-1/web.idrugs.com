c34a0ed914265b17848a5313b6c535fa25ca3809
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-06-02 18:31:51 -0300

    Add product / Add GET Products

---
+++ src/components/pages/Product.js
@@ -0,0 +1,34 @@
+import styles from "/Product.module.css";
+
+function Product() {
+    return(
+        <div>
+             <div className={styles.emp_card_actions}>
+                
+                <button onClick={openEditModal}>
+                    <BsPencil /> Editar
+                </button>
+                <Modal open={editModal} >
+                    <div>
+                        <ProdForm handleSubmit={handleEdit} btnText="Salvar" empData={product}/>
+                        <button onClick={closeEditModal}>Cancelar</button>
+                    </div>
+                </Modal>
+
+                
+                <button onClick={openDeleteModal}>
+                    <BsFillTrashFill /> Excluir
+                </button>
+                <Modal open={deleteModal} >
+                    <div>
+                        <p>Tem certeza que deseja excluir esse Produto ?</p>
+                        <button onClick={closeDeleteModal}>Cancelar</button>
+                        <button onClick={remove}>Excluir</button>
+                    </div>
+                </Modal>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
