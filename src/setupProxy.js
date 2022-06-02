fd832ac7656535a1c4c5ec754a2c25d0511060fc
Ítalo Moura <italo.moura1201@gmail.com>
2022-06-02 20:43:28 -0300

    add proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/idrugs-app/**', { target: ['https://idrugs-app.herokuapp.com', 'http://idrugs-app.herokuapp.com'] }));
+    app.use(createProxyMiddleware('/idrugs-app/**', { target: 'https://idrugs-app.herokuapp.com' }));
+    app.use(createProxyMiddleware('/idrugs-app/**', { target: 'http://idrugs-app.herokuapp.com' }));
 };
